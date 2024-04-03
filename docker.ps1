9288759fa4d94e04b1f4646cc87f39632824a537
DoganM95 <doganmermertas@hotmail.de>
2024-04-03 22:38:33 +0200

    prune dangling images

--- docker.ps1
+++ docker.ps1
@@ -1,6 +1,9 @@
 # Stop and remove any running image response cotainer
 docker ps -a | Select-String "image" | ForEach-Object { docker rm -f $_.ToString().Split(" ")[0] }
 
+# Remove dangling images
+docker image prune -f
+
 # Build the image
 docker build -t ip-image-server .
 
